681be77bf3461da032aa89186832ec5756f5b700
Vaibhav Patel <vaibhavpatel@Vaibhavs-MacBook-Air.local>
2018-03-11 18:28:38 -0500

    adding files for real this time

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+var cards = [ 
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+
+	if (cardsInPlay.length === 2, cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+	}
+	};
+
+var flipCard = function() {
+
+	var cardId = this.getAttribute('data-id');
+
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+
+	cardsInPlay.push(cards[cardId].rank);	
+
+	this.setAttribute('src', cards[cardId].cardImage);	
+
+	checkForMatch();
+	
+};
+
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', "images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click',flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+}
+}
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+
